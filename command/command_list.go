512b4a122ed4f989e585149b9d56ebeb4a32c4c8
jixwanwang <jixuan_wang@brown.edu>
2016-02-11 17:30:50 -0800

    oops forgot to check on command

--- command/command_list.go
+++ command/command_list.go
@@ -31,13 +31,17 @@ func (T *commandList) Response(username, message string, whisper bool) {
 		return
 	}
 
-	commands := "command \t| response"
-	for _, c := range T.cp.commands {
-		commands = fmt.Sprintf("%s\n%s\t| %s", commands, c.command, c.response)
-	}
+	clearance := T.cp.channel.GetLevel(username)
+	_, err = T.commands.parse(message, clearance)
+	if err == nil {
+		commands := "command \t| response"
+		for _, c := range T.cp.commands {
+			commands = fmt.Sprintf("%s\n%s\t| %s", commands, c.command, c.response)
+		}
 
-	paste := T.cp.pasteBin.Paste("Jixbot commands", commands, "1", "10M")
-	if len(paste) > 0 {
-		T.cp.Say(fmt.Sprintf("Current jixbot commands: %s", paste))
+		paste := T.cp.pasteBin.Paste("Jixbot commands", commands, "1", "10M")
+		if len(paste) > 0 {
+			T.cp.Say(fmt.Sprintf("Current jixbot commands: %s", paste))
+		}
 	}
 }
