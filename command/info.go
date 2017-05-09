6502b0728585d0f939e2e731e276fc9dd12cea08
jixwanwang <jixuan_wang@brown.edu>
2017-05-09 09:20:03 -0700

    better version method

--- command/info.go
+++ command/info.go
@@ -52,7 +52,7 @@ func (T *info) Response(username, message string, whisper bool) {
 
 	_, err = T.version.parse(message, clearance)
 	if err == nil {
-		T.cp.Say("/me is v3.3")
+		T.cp.Say("/me is v3.3. Most recent change: storing quotes and clips.")
 	}
 
 	_, err = T.aboot.parse(message, clearance)
